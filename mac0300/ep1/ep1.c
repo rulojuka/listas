a0651ad967719b2f25c4695785f1336fd153577a
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2015-09-13 12:43:30 -0300

    [MAC300] Aumentando nmax para evitar segfault

--- mac0300/ep1/ep1.c
+++ mac0300/ep1/ep1.c
@@ -15,15 +15,20 @@ int main(){
   double A[nmax][nmax];
   double b[nmax];
   int p[nmax];
+  int ret;
 
   le_entrada(&n, A, b);
 
   /* DESCOMENTE EXATAMENTE UMA DAS LINHAS ABAIXO */
 
-  /*printf("CHOL row:\n");  testa_cholesky(n,A,b,0,0,1);*/
-  /*printf("CHOL col:\n");  testa_cholesky(n,A,b,1,0,1);*/
-  /*printf("LU row:\n");  testa_lu(n,A,b,p,0,0,1);*/
-  printf("LU col:\n");  testa_lu(n,A,b,p,1,0,1);
+  printf("CHOL row:\n");  ret=testa_cholesky(n,A,b,0,0,1);
+  /*printf("CHOL col:\n");  ret=testa_cholesky(n,A,b,1,0,1);*/
+  /*printf("LU row:\n");  ret=testa_lu(n,A,b,p,0,0,1);*/
+  /*printf("LU col:\n");  ret=testa_lu(n,A,b,p,1,0,1);*/
+
+  if(ret==-1){
+    printf("A equacao nao pode ser resolvida.\n");
+  }
 
   return 0;
 }
